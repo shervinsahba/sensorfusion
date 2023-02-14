e22611f14a7002cd1f07129158fe0b58f67bec9c
Shervin Sahba <shervin@tuta.io>
2023-02-14 14:14:42 -0800

    renamed params. added 2nd dropout layer as option

--- src/SDNN.py
+++ src/SDNN.py
@@ -1,34 +1,35 @@
+import numpy as np
 from torch import nn,tensor
 from torch.utils.data import TensorDataset,DataLoader
 import torch.nn.functional as F
 from .torch_boilerplate import *
 
 class SDNN(nn.Module):
-    def __init__(self,input_size,output_size,
-                 l1_size=1024,l2_size=128,dropout_p=0.5):
+    def __init__(self,input_size,output_size,l1=1024,l2=128,dp1=0.10,dp2=0.05):
         super().__init__()
         self.input_size = input_size
-        self.l1_size = l1_size
-        self.l2_size = l2_size
         self.output_size = output_size
-        self.dropout_p = dropout_p
+        self.l1 = l1
+        self.l2 = l2
+        self.dp1 = dp1
+        self.dp2 = dp2
         
         self.learn_features = nn.Sequential(         
-            nn.Linear(input_size, self.l1_size),
+            nn.Linear(input_size, self.l1),
             nn.ReLU(inplace=True),
             # nn.PReLU(num_parameters=input_size), 
             nn.BatchNorm1d(1),  
             )
         
         self.learn_coef = nn.Sequential(            
-            nn.Linear(self.l1_size, self.l2_size),
+            nn.Linear(self.l1, self.l2),
             nn.ReLU(inplace=True),  
-            # nn.PReLU(num_parameters=l1_size),
+            # nn.PReLU(num_parameters=l1),
             nn.BatchNorm1d(1),  
             )
 
         self.learn_dictionary = nn.Sequential(
-            nn.Linear(self.l2_size, self.output_size),
+            nn.Linear(self.l2, self.output_size),
             )
         
         for m in self.modules():
@@ -44,8 +45,9 @@ class SDNN(nn.Module):
 
     def forward(self, xb):
         xb = self.learn_features(xb)
-        xb = F.dropout(xb, p=self.dropout_p, training=self.training)
+        xb = F.dropout(xb, p=self.dp1, training=self.training)
         xb = self.learn_coef(xb)
+        xb = F.dropout(xb, p=self.dp2, training=self.training)
         xb = self.learn_dictionary(xb) 
         return xb
 
@@ -53,10 +55,16 @@ class SDNN(nn.Module):
 def preprocess(x,y):
     dev = get_torchdevice()
     return x.unsqueeze(1).float().to(dev), \
-           y.unsqueeze(1).float().to(dev), \
+           y.unsqueeze(1).float().to(dev)
 
 
 def get_dataloader(x,y,bs,shuffle):    
     ds = TensorDataset(*map(tensor, (x,y)))
     dl = DataLoader(ds, batch_size=bs, shuffle=shuffle)
     return WrappedDataLoader(dl, preprocess)
+
+
+def predict(x, y, model):
+    r = model(tensor(x).float().unsqueeze(1)).detach().numpy().squeeze()
+    mse_loss = np.mean((r-y)**2)
+    return r, mse_loss
